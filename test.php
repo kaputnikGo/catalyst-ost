5652710c1b06a7f86ec311a467fedc8af7e7c964
Minty Fresh <kaputnikGo@gmail.com>
2017-07-20 16:47:10 +1000

    sort options, db entries, test to local

--- test.php
+++ test.php
@@ -7,25 +7,164 @@
    // columns spearated by a comma ($delimiter = ",")
 
    // ASSUMPTION email check will not check that domain name resolves to an actual mail server 
-   //domain, errors to be outputted to terminal
+   // domain, errors to be outputted to terminal
+
+   // ASSUMPTION lack of proper email means user is not entered into db, names must be present too
 
    // ASSUMPTION small csv file size means will use an array for pre-db
 
-   print "\n------------------------\n";
-   print "env info:\n";
-   $phpv = phpversion();
-   print "\nphp version: $phpv";
-   $mysqlv = getMySQLVersion();
-   print "\nmysql version: $mysqlv";
-   print "\n------------------------\n\n";
+   // ASSUMPTION spec is for a program that enters data into db, no mention of db edits etc.
+   // this program will therefore only add entries that do not already exist
+ 
 
-// UTILS
-   
+   //TODO
+      // global vars... pass by ref
+      // at DB entry strips ' from O'Really names
+      // sane and proper into classes
+      // sql checks
+      // cmdline options proper
+      // help display better info
+      // order of execution in php
+
+// VARS
+   // set up empty 2D array for entries of 3 fields
+   $entryArray = array(array("","",""));
+   $dbhost = "localhost"; // default, to be set by user
+   $dbuser = "catOST"; // default, to be set by user
+   $dbpass = "password"; // default, to be set by user
+   $dbname = "catalyst_ost"; // default
+   $dbtable = "users"; // set as per spec
+
+// SQL FUNCTIONS
    function getMySQLVersion() { 
       $output = shell_exec('mysql -V'); 
       preg_match('@[0-9]+\.[0-9]+\.[0-9]+@', $output, $version); 
       return $version[0]; 
    }
+
+   function createDB() {
+      global $dbhost;
+      global $dbuser;
+      global $dbpass;
+      global $dbname;
+      global $dbtable;
+
+      // TODO $db vars check
+
+      $sqlConn = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
+      if ($sqlConn->connect_error) {
+         die("\nSQL Connection failed: ".$sqlConn->connect_error)."\n";
+      }
+
+      $sqlCreate = "CREATE TABLE IF NOT EXISTS ".$dbtable." (
+         name VARCHAR(30) NOT NULL,
+         surname VARCHAR(30) NOT NULL,
+         email VARCHAR(50) NOT NULL,
+         CONSTRAINT UNIQUE (email)
+      )";
+
+      if ($sqlConn->query($sqlCreate) === TRUE) {
+         print "\ndb created.\n";
+         $sqlConn->close();
+         return TRUE;
+      }
+      else {
+         print "\nERROR - db not created: ".$sqlConn->error."\n";
+         $sqlConn->close();
+         return FALSE;
+      }
+
+   }
+ 
+   function createTable() {
+      // this is set via spec to be "users"
+      //TODO
+      // requires -h -u -p options from commandline,
+      // test version use default names for local
+
+      print "\n request to create table\n";
+      if (createDB()) {
+         // wow
+         print "\ncreated\n";
+         interceptExit();
+      }
+      else {
+         // sigh
+         print "\nfailed\n";
+         interceptExit();
+      }
+      
+   }
+
+   function prepEntriesDB() {
+      // remove headings entry
+      // check for null email, delete from array
+      // can option here for check and edit instead of delete
+      global $entryArray;
+      $counter = 0;
+   
+      foreach ($entryArray as $entry) {
+         if (($entry[0] === "name") 
+               && ($entry[1] === "surname") 
+               && ($entry[2] === "email")) {
+            print "\nheadings entry: ".$entryArray[$counter][0]."\n";
+            unset($entryArray[$counter]);
+         }
+         else if ($entry[2] === NULL) {
+            unset($entryArray[$counter]);
+         }
+         $counter++;
+      }
+      return TRUE;  
+   } 
+
+   function addEntries() {
+      // TODO assume has db, have set dbhost,dbuser,dbpass, has array
+      global $dbhost;
+      global $dbuser;
+      global $dbpass;
+      global $dbname;
+      global $dbtable;
+      global $entryArray;
+      
+      if (prepEntriesDB()) {
+         $checkNum = sizeof($entryArray); 
+         $counter = 0;
+
+         $sqlConn = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
+         if ($sqlConn->connect_error) {
+            die("\nSQL Connection failed: ".$sqlConn->connect_error)."\n";
+         }
+
+         $sqlStatement = $sqlConn->prepare("INSERT INTO ".$dbtable." (name, surname, email)".
+                    "VALUES (?, ?, ?)");
+         $sqlStatement->bind_param("sss", $name, $surname, $email);
+
+         foreach ($entryArray as $entry) {
+            $name = $entry[0];
+            $surname = $entry[1];
+            $email = $entry[2];
+            $sqlStatement->execute();
+            $counter++;
+         }
+         $sqlStatement->close();
+         $sqlConn->close();
+
+         //checker
+         if ($counter === $checkNum) {
+            print "DB entry number matches array size.\n";
+         }
+         else {
+            print "DB entry number mismatch (count, size): ".$counter.", ".$checkNum."\n";
+         }
+      }
+      else {
+         //
+      }
+   }
+   
+
+// UTILS 
 
    function interceptExit() {
       print "\nexiting...\n";
@@ -42,6 +181,15 @@
    function cleanSQLstring($str) {
       //TODO
       //$str = mysql_real_escape_string($str);
+   }
+
+   function filenameCheck($str) {
+      // check file is a .csv filename
+      if(strpos($str, ".csv") !== FALSE) {
+         return TRUE;
+      }
+      else 
+         return FALSE;
    }
 
    function nameFormatAnglo($str) {
@@ -66,47 +214,136 @@
          return NULL;
       }
    }
+ 
+
+
+
 
 // FUNCTIONS
-
-   function createTable() {
-      // this is set via spec to be "users"
-      print "\n request to create table\n";
-   }
-
-   function dryRun() {
-      // needs a filename to run, no DB actions
-      print "\n request to run non-DB functions on file.\n";
-   }
 
    function outputHelp() {
       // make a bit verbose for proper help...
       print "\n-----------------------------\n";
       print "USER DATABASE ENTRY PROGRAM\n";
       print "instructions for use:\n\n";
-      print "user_upload.php [--file][--create_table][--dry_run][-u][-p][-h][--help]\n";
-      print "--file [csv filename]\n".
-            "--create_table (creates db table no other action taken)\n".
+      print "user_upload.php [--file][--dry_run][h][-u][-p][--create_table][--help]\n";
+      print "--file [.csv filename]\n".          
             "--dry_run (used with --file, parses file for content but NO db actions\n".
+            "-h (sql host)\n".            
             "-u (sql username)\n".
             "-p (sql password)\n".
-            "-h (sql host)\n".
+            "--create_table (creates db table no other action taken)\n".
             "--help (print this menu)\n";
       print "\n-----------------------------\n\n";
    }
 
-   function parseFilename($str) {
+   function parseFilename($fileHandle) {
+      // this is the full option with file and db entry, 
+      // reqs: dbhost, dbuser, dbpass
       // check it first
-      print "\nfilename: ".$str."\n";
+
+      global $entryArray;
+
+      print "\nfilename: ".$fileHandle."\n";
+
+      // CHECK FILE
+      print "\nFile check: ";
+      
+      if (filenameCheck($fileHandle)) {
+         $rawFile = fopen($fileHandle, "r") or die("unable to open file: $fileHandle\n");
+         $lineNums = 0;
+
+         if ($rawFile) {
+            while (($line = fgets($rawFile)) !== false) {
+            
+               $entryArray[$lineNums] = explode(",", cleanStringInput($line));
+               $lineNums++;
+            }
+            fclose($rawFile);
+            print "OK. file closed after $lineNums lines read.\n\n";
+            processFileContents();
+         } 
+         else {
+            // error opening the file.
+            print ("error reading file\n");
+            interceptExit();
+         }
+      }
+      else {
+         print "unknown file type: ".$str;
+         interceptExit();
+      }
    }
+
+   function processFileContents() {
+      // according to spec first line of file is headings "name,surname,email"
+      global $entryArray;
+
+      $contentNum = sizeof($entryArray);
+      //ignore $fileContents[0] assume headings
+
+      print "\nProcess file contents, format entries:\n";
+
+      $lineNums = 0;
+      foreach($entryArray as $entry) {
+         
+         if ($lineNums == 0) { 
+            $lineNums++;
+            // skip first entry as it 'should' be the headings
+            continue; 
+         }
+         // first name
+         $entryArray[$lineNums][0] = nameFormatAnglo($entry[0]);
+         // surname
+         $entryArray[$lineNums][1] = nameFormatAnglo($entry[1]);
+         // email check, can return NULL
+         $entryArray[$lineNums][2] = simpleEmailCheck($entry[2]);
+         if ($entry[2] === NULL) {
+            // have error in email
+            // option to manually fix email?
+            print $entryArray[$lineNums][0].
+                  ",".$entryArray[$lineNums][1].
+                  ", INVALID EMAIL.\n";
+         }
+         else {
+            print $entryArray[$lineNums][0].
+                  ",".$entryArray[$lineNums][1].
+                  ",".$entryArray[$lineNums][2]."\n";
+         }
+         $lineNums++;
+      }
+      print "\nend of array print.\n\n";
+      addEntries();
+   }
+
+   function dryRun() {
+      global $entryArray;
+
+      // needs a file of entries to run, no DB actions
+      print "\nrequest to run non-DB functions on file.\n";
+      // check for valid entryArray
+      if (sizeof($entryArray) >= 2) {
+         // assume have at least one entry as well as headings
+         print "\nDry_run has an array to work with.\n";
+         processFileContents();
+      }
+      else {
+         print "\nDry_run did not find any entries to work with.\n";
+         outputHelp();
+         interceptExit();
+      }
+   }
+
    function parseSQLusername($str) {
       // check it first
       print "\nSQL username: ".$str."\n";
    }
+
    function parseSQLpassword($str) {
       // check it first and print teh passw0rdz!!
       print "\nSQL password: ".$str."\n";
    }
+
    function parseSQLhost($str) {
       // check it first
       print "\nSQL host: ".$str."\n";
@@ -126,11 +363,9 @@
                case "--file":
                   parseFilename($optionsArray[$optionNum + 1]);
                   break;
-               case "--create_table":
-                  print "\ncreate table option: ".$optionNum."\n";
-                  break;
                case "--dry_run":
-                  print "\ndry run option: ".$optionNum."\n";
+                  // check for filename
+                  dryRun();
                   break;
                case "-u":
                   parseSQLusername($optionsArray[$optionNum + 1]);
@@ -140,6 +375,9 @@
                   break;
                case "-h":
                   parseSQLhost($optionsArray[$optionNum + 1]);
+                  break;
+               case "--create_table":
+                  createTable();
                   break;
                case "--help":
                   outputHelp();
@@ -154,94 +392,47 @@
    }
 
 
-// CMDLINE ARGS
 
-   // $argc = number of arguments passed 
-   // $argv = arguments passed
-   // first $argc is for php exe filename
-   // need to look for switches (options) 
 
-   if ($argc <= 1) {
-      // no options found at cmdline
-      // autorun the help option
-      print "You need to add options for this program to run.\n\n";
-      outputHelp();
-      interceptExit();
-   }
-   else {
-      // just what options are used?
-      $argsCount = 0;
       $optionsArray = array("");
-      foreach($argv as $argStr) {
-         $optionsArray[$argsCount] = $argStr;
-         $argsCount++;
-      }
-      parseOptions($optionsArray);
-      interceptExit();
-   }
 
+      print "\n------------------------\n";
+      print "env info:\n";
+      $phpv = phpversion();
+      print "\nphp version: $phpv";
+      $mysqlv = getMySQLVersion();
+      print "\nmysql version: $mysqlv";
+      print "\n------------------------\n\n";
+   // CMDLINE ARGS
 
-// CHECK FILE HAS CONTENTS
-   print "\nFile check: ";
-   $rawFile = fopen($fileHandle, "r") or die("unable to open file: $fileHandle\n");
-   $lineNums = 0;
+      // $argc = number of arguments passed 
+      // $argv = arguments passed
+      // first $argc is for php exe filename
+      // need to look for switches (options) 
 
-   // set up empty 2D array for entries of 3 fields
-   $entryArray = array(array("","",""));
-
-   if ($rawFile) {
-      while (($line = fgets($rawFile)) !== false) {
-         
-         $entryArray[$lineNums] = explode(",", cleanStringInput($line));
-         $lineNums++;
-      }
-      fclose($rawFile);
-      print "OK. file closed after $lineNums lines read.\n\n";
-   } 
-   else {
-      // error opening the file.
-      print ("error reading file\n");
-   }
-
-// CONTENTS CHECK
-   // according to spec first line of file is headings "name,surname,email"
-   $contentNum = sizeof($entryArray);
-   print "Array check size: $contentNum\n";
-   //ignore $fileContents[0] assume its the headings
-
-   print "\nArray check cleaned and formatted entries:\n";
-
-   $lineNums = 0;
-   foreach($entryArray as $entry) {
-      
-      if ($lineNums == 0) { 
-         $lineNums++;
-         // skip first entry as it 'should' be the headings
-         continue; 
-      }
-      // first name
-      $entry[0] = nameFormatAnglo($entry[0]);
-      // surname
-      $entry[1] = nameFormatAnglo($entry[1]);
-      // email check, can return NULL
-      $entry[2] = simpleEmailCheck($entry[2]);
-      if ($entry[2] === NULL) {
-         // have error in email
-         // option to manually fix email?
-         print $entry[0].",".$entry[1].", INVALID EMAIL.\n";
-
+      if ($argc <= 1) {
+         // no options found at cmdline
+         // autorun the help option
+         print "You need to add options for this program to run.\n\n";
+         outputHelp();
+         interceptExit();
       }
       else {
-         print $entry[0].",".$entry[1].",".$entry[2]."\n";
+         // just what options are used?
+         $argsCount = 0;
+         //protected $optionsArray = array("");
+         foreach($argv as $argStr) {
+            $optionsArray[$argsCount] = $argStr;
+            $argsCount++;
+         }
+         parseOptions($optionsArray);
+   //TODO
+   // rem interceptExit() to allow program to continue 
+         //interceptExit();
       }
-   }
-
-   print "\nend of array print.\n\n";
-   
-
-// SQL DB
 
 
-// MAIN 
+
+
 
 ?>
